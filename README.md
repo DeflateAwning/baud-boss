8f88d0526f203cba858b8ece264c6c490721a9e3
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-04-02 22:13:57 -0600

    Add port listing and receiving functionality
    With this, the vision begins

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # baud-boss
 A feature-rich UART serial terminal, written in Rust
+
+## Use It
+
+```bash
+cargo install --git https://github.com/DeflateAwning/baud-boss
+# TODO: how to run it after installing like this?
+```
+
+## Features
+* print out incoming serial terminal content
+
+## Upcoming Features
+[ ] send messages character-by-character
+[ ] send messages by preparing them in an input box
+[ ] configurable EOL behaviour (TX)
+[ ] configurable EOL behaviour (RX)
+[ ] UI for selecting options, instead of requiring they be supplied by CLI args
+[ ] end-of-message character (optional)
+[ ] start-of-message timestamps (with configurable timeouts, maybe)
+[ ] installable via `cargo` and crates.io
+[ ] show incoming non-printable bytes (or all bytes) as hex
+[ ] send hex as raw bytes
+
+## Inspiration
+* [pyserial](https://github.com/pyserial/pyserial) (for its ubiquity)
+* [ttyper](https://github.com/max-niederman/ttyper) (for its UI)
---
+++ src/main.rs
@@ -0,0 +1,103 @@
+
+use std::path::PathBuf;
+use std::time::Duration;
+
+use clap::{Parser, Subcommand};
+
+use serialport5;
+use serialport5::SerialPort;
+
+use std::io::{Read, Write};
+
+/// A feature-rich UART serial terminal, written in Rust
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Opens the serial port for communication
+    OpenPort {
+        /// Baud rate for serial communication
+        #[arg(short = 'b', long, default_value_t = 115200)]
+        baud_rate: u32,
+
+        /// Serial port to use for communication
+        #[arg(short = 'p', long)]
+        serial_port: String,
+    },
+
+    /// List available serial ports, then exit
+    ListPorts,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Some(Commands::ListPorts) => {
+            list_ports();
+        }
+        Some(Commands::OpenPort { baud_rate, serial_port }) => {
+            open_serial_port_and_read(baud_rate, serial_port);
+        }
+        None => {
+            println!("No command provided. Exiting...");
+        }
+    }
+}
+
+fn list_ports() {
+    let ports = serialport5::available_ports().expect("No serial ports found!");
+
+    if ports.is_empty() {
+        println!("No serial ports found!");
+        return;
+    }
+
+    println!("Available serial ports ({} ports):", ports.len());
+    for (index, port) in ports.iter().enumerate() {
+        println!("--- Port #{}: {}", index, port.port_name);
+    }
+}
+
+fn open_serial_port_and_read(baud_rate: u32, serial_port: String) {
+
+    let mut port = SerialPort::builder()
+        .baud_rate(baud_rate)
+        .read_timeout(Some(Duration::from_millis(10)))
+        .open(serial_port.clone())
+        .expect("Failed to open port"); // FIXME
+
+    println!("Opened serial port '{}' at {} baud.", serial_port, baud_rate);
+
+    // Read from the serial port
+    loop {
+        let mut serial_buf: Vec<u8> = vec![0; 32];
+        let val = port.read(serial_buf.as_mut_slice());
+
+        match val {
+            Ok(bytes_read) => {
+                if bytes_read > 0 {
+                    let data = &serial_buf[..bytes_read];
+                    let data_str = std::str::from_utf8(data).unwrap();
+                    print!("{}", data_str);
+                }
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => {
+                // Do nothing
+                // TODO: consider printing an "end-of-stream" character here, marking the next one as the start of a new message (maybe with timestamp), etc.
+            }
+            Err(e) => {
+                eprintln!("Error reading from serial port: {}", e);
+                break;
+            }
+        }
+    }
+    // Do something with the serial port here
+}
