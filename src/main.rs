1cf805a73c76b2ce55cb32830d5eefb992b13495
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-04-03 20:15:12 -0600

    Refactor keypress handling, make it non-blocking

--- src/main.rs
+++ src/main.rs
@@ -69,148 +69,158 @@ fn run_app<B: Backend>(
     loop {
         terminal.draw(|f| ui(f, app))?;
 
-        
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Release {
-                // Skip events that are not KeyEventKind::Press
-                continue;
+        if let Ok(true) = event::poll(std::time::Duration::from_millis(100)) {
+            if let Event::Key(key) = event::read()? {
+                if key.kind != event::KeyEventKind::Release {
+                    // Skip events that are not KeyEventKind::Press
+                    
+                    // Handle KeyEventKind::Press events:
+                    if handle_keypresses(key, app) {
+                        break;
+                    }
+                }
             }
-            match app.current_screen {
-                CurrentScreen::PickSerialPort => {
-                    if is_keypress_quit_event(key, true) {
-                        break;
-                    }
-                    // TODO: handle up and down arrows
-                    match key.code {
-                        KeyCode::Char('j') | KeyCode::Down | KeyCode::Right => {
-                            app.pick_serial_port_list_state.next();
-                        }
-                        KeyCode::Char('k') | KeyCode::Up | KeyCode::Left => {
-                            app.pick_serial_port_list_state.previous();
-                        }
-                        KeyCode::Enter => {
-                            // read and store the selected serial port
-                            let selected_port = app.pick_serial_port_list_state.get_selected();
-                            match selected_port {
-                                Some(port) => {
-                                    app.selected_serial_port = Some(port);
-                                    app.current_screen = CurrentScreen::PickBaudRate;
-                                }
-                                None => {
-                                    app.selected_serial_port = None;
-                                    // don't change screens
-                                }
-                            }
-                        }
-                        _ => {}
-                    }
-                },
-
-                CurrentScreen::PickBaudRate => {
-                    if is_keypress_quit_event(key, true) {
-                        break;
-                    }
-                    match extract_modified_key(key) {
-                        ModifierWrapper::Control(KeyCode::Backspace) | ModifierWrapper::Control(KeyCode::Char('h')) => {
-                            // Ctrl+Backspace should clear the field; it comes in as Ctrl+H sometimes
-                            app.pick_baud_rate_input_field.clear();
-                        },
-                        _ => { }
-                    }
-                    match key.code {
-                        KeyCode::Char('b') => {
-                            // go back
-                            app.current_screen = CurrentScreen::PickSerialPort;
-                        }
-                        KeyCode::Char('c') => {
-                            // clear
-                            app.pick_baud_rate_input_field.clear();
-                        }
-                        KeyCode::Char(c) => {
-                            if c.is_ascii_digit() {
-                                // silly check to avoid writing a number with a leading zero
-                                if (app.pick_baud_rate_input_field.len() > 0)
-                                        || (app.pick_baud_rate_input_field.len() == 0 && c != '0'){
-                                    app.pick_baud_rate_input_field.push(c);
-                                }
-                            }
-                        }
-                        KeyCode::Backspace => {
-                            app.pick_baud_rate_input_field.pop();
-                        }
-                        KeyCode::Enter => {
-                            // store the baud rate
-                            let baud_rate = app.pick_baud_rate_input_field.parse::<u32>();
-                            match baud_rate {
-                                Ok(rate) => {
-                                    app.app_config.baud_rate = rate;
-                                    app.current_screen = CurrentScreen::Main;
-                                }
-                                Err(_) => {
-                                    // this shouldn't really happen, just clear the field and let them try again though
-                                    app.pick_baud_rate_input_field.clear();
-                                }
-                            }
-                        }
-                        _ => {}
-                    }
-                },
-
-                CurrentScreen::Main => {
-                    if is_keypress_quit_event(key, false) { // NOTE: don't quit on 'q'
-                        break;
-                    }
-
-                    match extract_modified_key(key) {
-                        ModifierWrapper::Control(KeyCode::Char('h')) => {
-                            app.current_screen = CurrentScreen::Help;
-                        }
-                        ModifierWrapper::Control(KeyCode::Char('b')) => {
-                            app.current_screen = CurrentScreen::PickBaudRate;
-                        }
-                        _ => {}
-                    }
-
-                    match key.code {
-                        KeyCode::Enter => {
-                            // TODO: send the data
-                        }
-                        _ => {}
-                    }
-                },
-
-                CurrentScreen::Config1 => {
-                    if is_keypress_quit_event(key, true) {
-                        break;
-                    }
-                    match key.code {
-                        KeyCode::Esc | KeyCode::Enter => {
-                            app.current_screen = CurrentScreen::Main;
-                        }
-                        // TODO: handle changing the config
-                        _ => {}
-                    }
-                },
-
-                CurrentScreen::Help => {
-                    if is_keypress_quit_event(key, true) {
-                        break;
-                    }
-                    match key.code {
-                        KeyCode::Esc => {
-                            app.current_screen = CurrentScreen::Main;
-                        }
-                        _ => {
-                            app.current_screen = CurrentScreen::Main;
-                        }
-                    }
-                },
-
-            }
-
         }
     }
     Ok(())
+}
+
+/// Handle keypresses for the app (next-screen logic, quit logic, input logic, etc.)
+/// Returns true if the app should exit
+fn handle_keypresses(key: KeyEvent, app: &mut App) -> bool {
+    match app.current_screen {
+        CurrentScreen::PickSerialPort => {
+            if is_keypress_quit_event(key, true) {
+                return true; // exit program
+            }
+            // TODO: handle up and down arrows
+            match key.code {
+                KeyCode::Char('j') | KeyCode::Down | KeyCode::Right => {
+                    app.pick_serial_port_list_state.next();
+                }
+                KeyCode::Char('k') | KeyCode::Up | KeyCode::Left => {
+                    app.pick_serial_port_list_state.previous();
+                }
+                KeyCode::Enter => {
+                    // read and store the selected serial port
+                    let selected_port = app.pick_serial_port_list_state.get_selected();
+                    match selected_port {
+                        Some(port) => {
+                            app.selected_serial_port = Some(port);
+                            app.current_screen = CurrentScreen::PickBaudRate;
+                        }
+                        None => {
+                            app.selected_serial_port = None;
+                            // don't change screens
+                        }
+                    }
+                }
+                _ => {}
+            }
+        },
+
+        CurrentScreen::PickBaudRate => {
+            if is_keypress_quit_event(key, true) {
+                return true; // exit program
+            }
+            match extract_modified_key(key) {
+                ModifierWrapper::Control(KeyCode::Backspace) | ModifierWrapper::Control(KeyCode::Char('h')) => {
+                    // Ctrl+Backspace should clear the field; it comes in as Ctrl+H sometimes
+                    app.pick_baud_rate_input_field.clear();
+                },
+                _ => { }
+            }
+            match key.code {
+                KeyCode::Char('b') => {
+                    // go back
+                    app.current_screen = CurrentScreen::PickSerialPort;
+                }
+                KeyCode::Char('c') => {
+                    // clear
+                    app.pick_baud_rate_input_field.clear();
+                }
+                KeyCode::Char(c) => {
+                    if c.is_ascii_digit() {
+                        // silly check to avoid writing a number with a leading zero
+                        if (app.pick_baud_rate_input_field.len() > 0)
+                                || (app.pick_baud_rate_input_field.len() == 0 && c != '0'){
+                            app.pick_baud_rate_input_field.push(c);
+                        }
+                    }
+                }
+                KeyCode::Backspace => {
+                    app.pick_baud_rate_input_field.pop();
+                }
+                KeyCode::Enter => {
+                    // store the baud rate
+                    let baud_rate = app.pick_baud_rate_input_field.parse::<u32>();
+                    match baud_rate {
+                        Ok(rate) => {
+                            app.app_config.baud_rate = rate;
+                            app.current_screen = CurrentScreen::Main;
+                        }
+                        Err(_) => {
+                            // this shouldn't really happen, just clear the field and let them try again though
+                            app.pick_baud_rate_input_field.clear();
+                        }
+                    }
+                }
+                _ => {}
+            }
+        },
+
+        CurrentScreen::Main => {
+            if is_keypress_quit_event(key, false) { // NOTE: don't quit on 'q'
+                return true; // exit program
+            }
+
+            match extract_modified_key(key) {
+                ModifierWrapper::Control(KeyCode::Char('h')) => {
+                    app.current_screen = CurrentScreen::Help;
+                }
+                ModifierWrapper::Control(KeyCode::Char('b')) => {
+                    app.current_screen = CurrentScreen::PickBaudRate;
+                }
+                _ => {}
+            }
+
+            match key.code {
+                KeyCode::Enter => {
+                    // TODO: send the data
+                }
+                _ => {}
+            }
+        },
+
+        CurrentScreen::Config1 => {
+            if is_keypress_quit_event(key, true) {
+                return true; // exit program
+            }
+            match key.code {
+                KeyCode::Esc | KeyCode::Enter => {
+                    app.current_screen = CurrentScreen::Main;
+                }
+                // TODO: handle changing the config
+                _ => {}
+            }
+        },
+
+        CurrentScreen::Help => {
+            if is_keypress_quit_event(key, true) {
+                return true; // exit program
+            }
+            match key.code {
+                KeyCode::Esc => {
+                    app.current_screen = CurrentScreen::Main;
+                }
+                _ => {
+                    app.current_screen = CurrentScreen::Main;
+                }
+            }
+        },
+
+    };
+    false
 }
 
 fn is_keypress_quit_event(key: KeyEvent, is_q_quit: bool) -> bool {
